e2488a9281bb410f36461976bde7506b36a6b479
pazamelin <pazamelin@edu.hse.ru>
2021-04-29 17:54:48 +0300

    f

---
+++ lab2/unit_tests.py
@@ -0,0 +1,24 @@
+from knapsack.problem import KnapsackProblem, KnapsackSolution, KnapsackItem
+from knapsack.branch_and_bound import branch_and_bound
+
+
+def bnb_unit_1():
+    task = KnapsackProblem(capacity=50,
+                           prices=[70, 20, 39, 37, 7, 5, 10],
+                           weights=[31, 10, 20, 19, 4, 3, 6])
+
+    solution = branch_and_bound(task)
+    assert solution.profit == 107
+
+
+def bnb_unit_2():
+    task = KnapsackProblem(capacity=10,
+                           prices=[40, 50, 100, 95, 30],
+                           weights=[2, 3, 2, 5, 3])
+
+    solution = branch_and_bound(task)
+    assert solution.profit == 245
+
+
+bnb_unit_1()
+bnb_unit_2()
